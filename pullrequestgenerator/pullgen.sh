279fd006b5662858d90ea4c658b1c8dcf432f6bf
Gopesh Chaudhary <gopesh.chaudhary@thingsprogramming.com>
2018-04-17 15:52:49 +0530

    Fixed Float with ceil

--- pullrequestgenerator/pullgen.sh
+++ pullrequestgenerator/pullgen.sh
@@ -37,10 +37,13 @@ BITBUCKET_BRANCHS="https://api.bitbucket.org/2.0/repositories/$BBPR_ID/$BBPR_REP
 BITBUCKET_PULLREQUEST="https://api.bitbucket.org/2.0/repositories/$BBPR_ID/$BBPR_REPO/pullrequests"
 ##############################--PATCHING --- ############################################
 $(curl -sS -H "Content-Type: application/json" -u $BBPR_EMAIL:$BBPR_PASS  $BITBUCKET_BRANCHS > $BRANCH_FILE)
-SIZE=$(grep -Po '"size": \d+' $BRANCH_FILE | sed 's/"size": \(\)/\1/g')
-PAGELEN=$(grep -Po '"pagelen": \d+' $BRANCH_FILE | sed 's/"pagelen": \(\)/\1/g')
-echo $SIZE
-echo $PAGELEN
+SIZE=10 #$(grep -Po '"size": \d+' $BRANCH_FILE | sed 's/"size": \(\)/\1/g')
+PAGELEN=10 #$(grep -Po '"pagelen": \d+' $BRANCH_FILE | sed 's/"pagelen": \(\)/\1/g')
+echo "Total is $SIZE"
+echo "Max Per Page is $PAGELEN"
+printf "%.0f\n"  $(echo "$SIZE/$PAGELEN" | bc -l) 
+echo `/usr/bin/perl -w -e "use POSIX; print ceil($SIZE/$PAGELEN), qq{\n}"`
+ 
 exit 1;
 $(curl -sS -H "Content-Type: application/json" -u $BBPR_EMAIL:$BBPR_PASS  $BITBUCKET_BRANCHS |grep -P '"type": "branch", "name": "release[\w\/.]+",' -o | sed 's/"type": "branch", "name": "\([[:alpha:][:digit:]\/\.]\+\)",/\1/g' >  $BRANCH_FILE)
 if grep -qF "$CURRBRANCH" $BRANCH_FILE;then
